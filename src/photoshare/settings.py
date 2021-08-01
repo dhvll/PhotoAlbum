19a5442dd82b682a0e23845c80ed438195b8a7b0
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2021-08-01 13:40:23 +0530

    fix error

--- src/photoshare/settings.py
+++ src/photoshare/settings.py
@@ -80,8 +80,8 @@ WSGI_APPLICATION = 'photoshare.wsgi.application'
 
 DATABASES = {
     'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'dhaval',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
